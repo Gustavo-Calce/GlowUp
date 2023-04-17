bd4aa30e35ddbbcd3937df4317875b4febe53093
Júlia <juliammneves@gmail.com>
2023-04-16 23:51:34 -0300

    Sprint2 - Front

---
+++ script.js
@@ -0,0 +1,12 @@
+/* let searchBtn = document.querySelector('searchBtn');
+let closeBtn = document.querySelector('closeBtn');
+let searchBox = document.querySelector('searchBox');
+
+
+
+searchBtn.onclick = function execute(){
+    searchBox.a('active')
+}
+closeBtn.onclick = function close(){
+    searchBox.classList.remove('active')
+}  */
